b3a08fb28117f952ae9a5ac757ea66238f501e26
r3dcube <wbwoodard@gmail.com>
2015-09-03 14:01:05 -0400

    Cleanup
    Cleaned out some extra code at the bottom so it wouldn't get run. Threw
    in a few comments to tidy things up.

--- particleToCurve.py
+++ particleToCurve.py
@@ -1,3 +1,14 @@
+###########################################################################
+#name:				particleTOCurve -- Oct 2015
+#Created by: Bryan Woodard & Chris Jones(github.com/csjones)
+#This script will build a curve and attach a locator to the
+#curve using the pathAnimation command (motion path)
+#
+#Use: Select nParticle in scene and run the script.
+#
+#Tested on Maya 2016 for Windows
+#Use at your own risk
+###########################################################################
 import maya.cmds as mc
 
 allParticleDictionary = {}
@@ -25,7 +36,7 @@ for currentFrame in range(0, int(maxFrames)):
 
 			particleDictionary[currentFrame] = particlesPosition
 			allParticleDictionary[str(particleName[0])] = particleDictionary
-			
+
 for curveParticleId in allParticleDictionary.keys():
 	#print sorted(allParticleDictionary[curveParticleId].keys())
 	#print curveParticleId
@@ -35,17 +46,10 @@ for curveParticleId in allParticleDictionary.keys():
 	if len(sortedKeyFrameList) > 1:
 
 		for keyFrame in sortedKeyFrameList:
-			pointList.append(allParticleDictionary[curveParticleId][keyFrame])		
+			pointList.append(allParticleDictionary[curveParticleId][keyFrame])
 		curveName = "partiCurve" + str(curveParticleId)
 		curveObj = mc.curve(name = curveName, p = pointList)
 		locName = "locatorName" + str(curveParticleId)
 		locObj = mc.spaceLocator(name = locName)
 		mc.pathAnimation(locObj, stu=sortedKeyFrameList[0], etu=sortedKeyFrameList[-1] ,c=curveObj)
 		makeBubble = mc.polyCube(name="bubble",w=.1,h=.1, d=.1, sx=5, sy=5, sz=5)
-		
-		#print pointList
-
-makeBubble = mc.polyCube(name="bubble",w=.1,h=.1, d=.1, sx=5, sy=5, sz=5)
-mc.sculpt(makeBubble, maxDisplacement=.1)
-mc.delete(makeBubble, ch=True)
-mc.nClothCreate()
